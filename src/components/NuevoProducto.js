b1613f2ea442e1811f028c3215edfe09eec1e4a5
ArechuaDaniel <daniel.ar98@hotmail.com>
2022-06-05 23:38:26 -0500

    first commit

---
+++ src/components/NuevoProducto.js
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+
+// Action de Redux
+import {crearNuevoProductoAction} from '../actions/productoAction'
+
+const NuevoProducto = ({history}) => {
+  // State del componente
+  const[nombre, guardarNombre] = useState('')
+  const[precio, guardarPrecio] = useState(0)
+
+  // Utilizar use dispatch y te crea una función
+  const dispatch = useDispatch();
+
+  // Acceder al state del store
+  const cargando = useSelector(state => state.productos.loading);
+  const error = useSelector(state => state.productos.error)
+
+  // Mandar llamar el action de productoAction
+  const agregarProducto = (producto) => dispatch(crearNuevoProductoAction(producto));
+
+  // Cuando el usuario haga submit
+  const submitNuevoProducto = (e) => {
+    e.preventDefault();
+
+    // Validar formulario
+    if (nombre.trim() === '' || precio <= 0) {
+      return;
+    }
+    // si no hay errores
+
+    // crear el nuevo producto
+    agregarProducto({
+      nombre,
+      precio
+    });
+
+    // redirecionar
+    history.push('/');
+  }
+  return (
+    <div className='row justify-content-center'>
+      <div className='col-md-8'>
+        <div className='card'>
+          <div className='card-body'>
+            <h2 className='text-center mb-4 font-weight-bold'>
+              Agregar Nuevo Producto
+            </h2>
+            <form
+              onSubmit={submitNuevoProducto}
+            >
+              <div className='form-group'>
+                <label htmlFor='nombre'>Nombre Producto</label>
+                <input 
+                  type='text'
+                  id='nombre'
+                  className='form-control'
+                  placeholder='Nombre Producto'
+                  name='nombre'
+                  value={nombre}
+                  onChange={e => guardarNombre(e.target.value)}
+                />
+              </div>
+              <div className='form-group'>
+                <label htmlFor='precio'>Precio Producto</label>
+                <input 
+                  type='number'
+                  id='precio'
+                  className='form-control'
+                  placeholder='Precio Producto'
+                  name='precio'
+                  value={precio}
+                  onChange={e => guardarPrecio(Number(e.target.value))}
+                />
+              </div>
+              <button
+                type='submit'
+                className='btn btn-primary font-weight-bold text-uppercase d-block w-100'
+              >
+                Agregar
+              </button>
+            </form>
+            {cargando ? <p>Cargando...</p> : null }
+            {error ? <p className='alert alert-danger p2 mt-4 text-center'>Hubo un error</p> : null }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NuevoProducto
